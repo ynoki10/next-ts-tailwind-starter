f4b9d2b88e8f77eacd6f9f8454626d2afa708c39
ynoki10 <sakanafront@gmail.com>
2024-02-03 22:01:55 +0900

    react/jsx-sort-props を追加

--- app/page.tsx
+++ app/page.tsx
@@ -12,31 +12,31 @@ export default function Home() {
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
-            By <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={100} height={24} priority />
+            By <Image alt="Vercel Logo" className="dark:invert" height={24} priority src="/vercel.svg" width={100} />
           </a>
         </div>
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
           alt="Next.js Logo"
-          width={180}
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           height={37}
           priority
+          src="/next.svg"
+          width={180}
         />
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          href="https://nextjs.org/docs"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{' '}
@@ -50,10 +50,10 @@ export default function Home() {
         </a>
 
         <a
-          href="https://nextjs.org/learn"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          href="https://nextjs.org/learn"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Learn{' '}
@@ -67,10 +67,10 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/templates"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          href="https://vercel.com/templates"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Templates{' '}
@@ -82,10 +82,10 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          href="https://vercel.com/new"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{' '}
